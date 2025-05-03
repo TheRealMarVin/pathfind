cf993aeeee38d63f926b90f2fd236b95de49bdc9
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-03 08:29:53 -0400

    make a difference between start and current

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ GRID_HEIGHT = 40            # number of cells vertically
 CELL_SIZE = 5              # pixel size of each cell
 FPS = 30                    # frames per second for the game loop
 
-NUM_STATIC_AREAS = 3        # number of static (non-moving) obstacle groups
+NUM_STATIC_AREAS = 13        # number of static (non-moving) obstacle groups
 NUM_DYNAMIC_AREAS = 4       # number of moving obstacle groups
 UPDATE_INTERVAL = 500       # ms between dynamic obstacle moves
 
@@ -11,6 +11,7 @@ UPDATE_INTERVAL = 500       # ms between dynamic obstacle moves
 COLOR_BG = (255, 255, 255)       # white background
 COLOR_OBSTACLE = (0, 0, 0)       # black obstacles
 COLOR_GRID_LINE = (200, 200, 200)
-COLOR_ROBOT = (0, 255, 0)        # green for robot
+COLOR_START = (0, 255, 0)        # green for start
+COLOR_ROBOT = (0, 0, 255)        # blue current position
 COLOR_GOAL = (255, 0, 0)         # red for goal
 COLOR_EROSION = (255, 165, 0)    # orange for erosion boundary
