4f8a1918caa52d000aaa8b60bd3ceb3990603cd4
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-08 06:53:18 -0400

    use seed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import argparse
 import pygame
+import random
 
 from constants import GRID_WIDTH, GRID_HEIGHT, CELL_SIZE, COLOR_BG, FPS
 from game import Game
@@ -27,4 +29,18 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Configuration to launch the small game and configure the bot.")
+    parser.add_argument("--seed", type=int, help="Seed for the random number generator")
+    parser.add_argument("--config", type=str, default=None, help="Config file to determine the experimentation to run")
+
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        seed_value = args.seed
+    else:
+        seed_value = random.randint(0, 2 ** 32 - 1)
+
+    random.seed(seed_value)
+    print("seed_value", seed_value)
+
     main()
