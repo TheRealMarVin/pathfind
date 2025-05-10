7ff8109122f8240deaa354461b500dc4f126c1fc
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-10 06:36:53 -0400

    preparing to replace constants by configs

---
+++ config.py
@@ -0,0 +1,19 @@
+import sys
+from pathlib import Path
+import yaml
+from readtheyaml.exceptions.validation_error import ValidationError
+from readtheyaml.schema import Schema
+
+CONFIG = None
+
+def load_config(config_path, schema):
+    global CONFIG
+    try:
+        schema = Schema.from_yaml(schema, Path("./configs/schemas"))
+        CONFIG = schema.validate_file(config_path)
+    except ValidationError as e:
+        print(f"❌ Validation failed: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    return CONFIG
+
