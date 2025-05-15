93e6e6a3e59a63ecac483128b7a23aea3d0194b6
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-15 18:51:53 -0400

    seed is in the config now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import pygame
-import random
 
 from config import load_config
 from game import Game
@@ -37,18 +36,10 @@ def main(config):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Configuration to launch the small game and configure the bot.")
-    parser.add_argument("--seed", type=int, help="Seed for the random number generator")
     parser.add_argument("--config", type=str, default="configs/simple_config.yaml", help="Config file to determine the experimentation to run")
     parser.add_argument("--schema", type=str, default="configs/schemas/project_schema.yaml", help="Schema file to validate the config")
-
     args = parser.parse_args()
 
-    if args.seed is not None:
-        seed_value = args.seed
-    else:
-        seed_value = random.randint(0, 2 ** 32 - 1)
-
-    random.seed(seed_value)
     config = load_config(args.config, args.schema)
 
     main(config)
