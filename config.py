191c93d762f5b6a8429018f5b94c2382a7b82ec7
therealmarvin <nakedtoothbrush@hotmail.com>
2025-05-20 06:15:56 -0400

    can skip schema if forced

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
 
 from pathlib import Path
+
+import yaml
 from readtheyaml.exceptions.validation_error import ValidationError
 from readtheyaml.schema import Schema
 
@@ -8,12 +10,16 @@ CONFIG = None
 
 def load_config(config_path, schema):
     global CONFIG
-    try:
-        schema = Schema.from_yaml(schema, Path("./configs/schemas"))
-        CONFIG = schema.validate_file(config_path)
-    except ValidationError as e:
-        print(f"❌ Validation failed: {e}", file=sys.stderr)
-        sys.exit(1)
+    if schema is None or schema == "":
+        with open(config_path, "r", encoding="utf-8") as f:
+            CONFIG = yaml.safe_load(f)
+    else:
+        try:
+            schema = Schema.from_yaml(schema, Path("./configs/schemas"))
+            CONFIG = schema.validate_file(config_path)
+        except ValidationError as e:
+            print(f"❌ Validation failed: {e}", file=sys.stderr)
+            sys.exit(1)
 
     return CONFIG
 
