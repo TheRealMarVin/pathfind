ab3b7cd3544db9f61f25f05eab373d83fa8f36ae
therealmarvin <nakedtoothbrush@hotmail.com>
2025-06-15 13:32:28 -0400

    start moving toward parallel generation. Code is in a discuting state now

--- game.py
+++ game.py
@@ -7,7 +7,6 @@ import yaml
 import config
 import numpy as np
 import pygame
-import random
 
 from datetime import datetime
 from agents.a_star_agent import AStarAgent
@@ -16,93 +15,87 @@ from environment.map import Map
 
 
 class Game:
-    def __init__(self):
-        if config.CONFIG["seed"] is not None:
-            map_seed = config.CONFIG["seed"]
-        else:
-            map_seed = random.randint(0, 99999)
+    def __init__(self, task):
+        self.task = task
 
-        self.map_seed = map_seed
-
-        self.agent_types = config.CONFIG["game"]["agent_types"]
-        self.maps_to_test = config.CONFIG["game"]["maps_to_test"]
-        self.spawns_per_map = config.CONFIG["game"]["spawns_per_map"]
         self.update_interval = config.CONFIG["game"]["update_interval"]
         self.color_goal = config.CONFIG["game"]["color_goal"]
         self.color_agent = config.CONFIG["game"]["color_agent"]
-
-        self.current_map_index = 0
-        self.current_spawn_index = 0
-        self.current_agent_index = 0
-
-        self.map = None
-        self.agent = None
-        self.spawn_data = {}
-        self.last_update = 0
         self.cell_size = config.CONFIG["map"]["cell_size"]
 
-        self.agent_traces = []
-        self.map_traces={}
+        # self.current_map_index = 0
+        # self.current_spawn_index = 0
+        # self.current_agent_index = 0
 
-        self._init_map()
+        self.map = task.map
+        self.agent = None
+        self.spawn_data = task.position_pairs
+        self.last_update = 0
+
+        # self.agent_traces = []
+        # self.map_traces={}
+        self.map_trace = {"seed": task.seed, "grid":self.map.grid.tolist(), "erosion":self.map.erosion.tolist(),
+                          "agent_type": task.agent_type}
+
+        # self._init_map()
         self._spawn_agent()
 
-    def _init_map(self):
-        seed = self.map_seed + self.current_map_index
-
-        self.random_generator = random.Random(seed)
-        np.random.seed(seed)
-
-        self.map = Map(
-            config.CONFIG["map"]["grid_width"],
-            config.CONFIG["map"]["grid_height"],
-            self.random_generator,
-            config.CONFIG["map"]["num_static_areas"],
-            config.CONFIG["map"]["num_dynamic_areas"]
-        )
-
-        free_positions = self.map.get_free_positions()
-        max_possible_pairs = len(free_positions) * (len(free_positions) - 1)  # n * (n-1) since order matters
-        spawns_per_map = min(self.spawns_per_map, max_possible_pairs)
-
-        if spawns_per_map < self.spawns_per_map:
-            print(
-                f"Warning: Reduced spawns_per_map from {self.spawns_per_map} to {spawns_per_map} to respect map size constraints")
-
-        max_attempts = 250
-        self.spawn_data.clear()
-        used_pairs = set()
-        
-        for i in range(spawns_per_map):
-            attempts = 0
-            while attempts < max_attempts:
-                # Randomly select start and goal from free positions
-                start_idx, goal_idx = np.random.choice(len(free_positions), 2, replace=False)
-                start = free_positions[start_idx]
-                goal = free_positions[goal_idx]
-                
-                # Create a unique key for this pair (order matters)
-                pair_key = (start, goal)
-                
-                if pair_key not in used_pairs:
-                    used_pairs.add(pair_key)
-                    self.spawn_data[i] = {"start": start, "goal": goal}
-                    break
-                    
-                attempts += 1
-            else:
-                print(f"Warning: Could not find unique start-goal pair after {max_attempts} attempts")
-                break
-
-        self.map_traces[self.current_map_index] = {"index":self.current_map_index, "seed":seed,
-                                                   "grid":self.map.grid.tolist(), "erosion":self.map.erosion.tolist()}
+    # def _init_map(self):
+    #     seed = self.map_seed + self.current_map_index
+    #
+    #     self.random_generator = random.Random(seed)
+    #     np.random.seed(seed)
+    #
+    #     self.map = Map(
+    #         config.CONFIG["map"]["grid_width"],
+    #         config.CONFIG["map"]["grid_height"],
+    #         self.random_generator,
+    #         config.CONFIG["map"]["num_static_areas"],
+    #         config.CONFIG["map"]["num_dynamic_areas"]
+    #     )
+    #
+    #     free_positions = self.map.get_free_positions()
+    #     max_possible_pairs = len(free_positions) * (len(free_positions) - 1)  # n * (n-1) since order matters
+    #     spawns_per_map = min(self.spawns_per_map, max_possible_pairs)
+    #
+    #     if spawns_per_map < self.spawns_per_map:
+    #         print(
+    #             f"Warning: Reduced spawns_per_map from {self.spawns_per_map} to {spawns_per_map} to respect map size constraints")
+    #
+    #     max_attempts = 250
+    #     self.spawn_data.clear()
+    #     used_pairs = set()
+    #
+    #     for i in range(spawns_per_map):
+    #         attempts = 0
+    #         while attempts < max_attempts:
+    #             # Randomly select start and goal from free positions
+    #             start_idx, goal_idx = np.random.choice(len(free_positions), 2, replace=False)
+    #             start = free_positions[start_idx]
+    #             goal = free_positions[goal_idx]
+    #
+    #             # Create a unique key for this pair (order matters)
+    #             pair_key = (start, goal)
+    #
+    #             if pair_key not in used_pairs:
+    #                 used_pairs.add(pair_key)
+    #                 self.spawn_data[i] = {"start": start, "goal": goal}
+    #                 break
+    #
+    #             attempts += 1
+    #         else:
+    #             print(f"Warning: Could not find unique start-goal pair after {max_attempts} attempts")
+    #             break
+    #
+    #     self.map_traces[self.current_map_index] = {"index":self.current_map_index, "seed":seed,
+    #                                                "grid":self.map.grid.tolist(), "erosion":self.map.erosion.tolist()}
 
     def _spawn_agent(self):
-        key = self.current_spawn_index
-        start_pos = self.spawn_data[key]["start"]
-        goal_pos = self.spawn_data[key]["goal"]
+        start_pos = self.spawn_data["start"]
+        goal_pos = self.spawn_data["goal"]
 
-        agent_type = self.agent_types[self.current_agent_index % len(self.agent_types)]
+        # TODO make a factory
+        agent_type = self.task.agent_type
 
         if agent_type == "astar":
             self.agent = AStarAgent(start_pos, goal_pos)
@@ -111,7 +104,7 @@ class Game:
         else:
             raise ValueError(f"Unknown agent type: {agent_type}")
 
-        print(f"Agent: {agent_type}, Map #{self.current_map_index + 1}, Spawn #{self.current_spawn_index + 1}\n  Start: {start_pos} -> Goal: {goal_pos}\n")
+        print(f"Agent: {agent_type}, Map #{self.task.map_index}, Spawn #{self.task.position_index}\n  Start: {start_pos} -> Goal: {goal_pos}\n")
 
     def update(self):
         now = pygame.time.get_ticks()
@@ -120,35 +113,42 @@ class Game:
             self.map.update(self.agent.position)
             self.agent.update(self.map)
 
-        if self.agent.has_reached_goal():
-            self._on_goal_reached()
+        # if self.agent.has_reached_goal():
+        #     self._on_goal_reached()
 
-    def _on_goal_reached(self):
-        self.agent_traces.append({"agent_index": self.current_agent_index, "spawn_index": self.current_spawn_index, "map_index": self.current_map_index, "agent_visited": copy.deepcopy(self.agent.visited), "agent_explored": copy.deepcopy(list(self.agent.explored))})
-        self.current_agent_index += 1
+    def is_task_completed(self):
+        return self.agent.has_reached_goal()
 
-        if self.current_agent_index >= len(self.agent_types):
-            self.current_agent_index = 0
-            self.current_spawn_index += 1
+    def get_trace(self):
+        agent_trace = {"agent_type": self.task.agent_type, "spawn_index": self.task.position_index, "map_index": self.task.map_index, "agent_visited": copy.deepcopy(self.agent.visited), "agent_explored": copy.deepcopy(list(self.agent.explored))}
+        return agent_trace, self.map_trace
 
-            if self.current_spawn_index >= self.spawns_per_map:
-                self.current_spawn_index = 0
-                self.current_map_index += 1
-
-                if self.current_map_index >= self.maps_to_test:
-                    self.export_runtime_data()
-
-                    pygame.quit()
-                    raise SystemExit("And... it's done!")
-
-                self._init_map()
-            else:
-                self.map.reset()
-
-        else:
-            self.map.reset()
-
-        self._spawn_agent()
+    # def _on_goal_reached(self):
+    #     self.agent_traces.append({"agent_index": self.current_agent_index, "spawn_index": self.current_spawn_index, "map_index": self.current_map_index, "agent_visited": copy.deepcopy(self.agent.visited), "agent_explored": copy.deepcopy(list(self.agent.explored))})
+    #     self.current_agent_index += 1
+    #
+    #     if self.current_agent_index >= len(self.agent_types):
+    #         self.current_agent_index = 0
+    #         self.current_spawn_index += 1
+    #
+    #         if self.current_spawn_index >= self.spawns_per_map:
+    #             self.current_spawn_index = 0
+    #             self.current_map_index += 1
+    #
+    #             if self.current_map_index >= self.maps_to_test:
+    #                 self.export_runtime_data()
+    #
+    #                 pygame.quit()
+    #                 raise SystemExit("And... it's done!")
+    #
+    #             self._init_map()
+    #         else:
+    #             self.map.reset()
+    #
+    #     else:
+    #         self.map.reset()
+    #
+    #     self._spawn_agent()
 
     def draw(self, surface):
         self.agent.draw(surface)
@@ -170,18 +170,18 @@ class Game:
             pygame.Rect(ax * self.cell_size, ay * self.cell_size, self.cell_size, self.cell_size)
         )
 
-    def export_runtime_data(self):
-        name = config.CONFIG['experiment_name']
-
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        output_dir = os.path.join("outputs", name + "-" + timestamp)
-        os.makedirs(output_dir, exist_ok=True)
-
-        with open(os.path.join(output_dir, "agent_output.json"), "w") as f:
-            json.dump(self.agent_traces, f, indent=4)
-
-        with open(os.path.join(output_dir, "map_output.json"), "w") as f:
-            json.dump(self.map_traces, f, indent=4)
-
-        with open(os.path.join(output_dir, "used_config.yaml"), "w") as f:
-            yaml.dump(config.CONFIG, f, default_flow_style=False, sort_keys=False)
+    # def export_runtime_data(self):
+    #     name = config.CONFIG['experiment_name']
+    #
+    #     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    #     output_dir = os.path.join("outputs", name + "-" + timestamp)
+    #     os.makedirs(output_dir, exist_ok=True)
+    #
+    #     with open(os.path.join(output_dir, "agent_output.json"), "w") as f:
+    #         json.dump(self.agent_traces, f, indent=4)
+    #
+    #     with open(os.path.join(output_dir, "map_output.json"), "w") as f:
+    #         json.dump(self.map_traces, f, indent=4)
+    #
+    #     with open(os.path.join(output_dir, "used_config.yaml"), "w") as f:
+    #         yaml.dump(config.CONFIG, f, default_flow_style=False, sort_keys=False)
--- main.py
+++ main.py
@@ -1,20 +1,148 @@
 import argparse
+import copy
+import json
+import numpy as np
+import os
 import pygame
+import random
+import yaml
 
+from datetime import datetime
+from tqdm import tqdm
+
+import config
 from config import load_config
+from environment.map import Map
 from game import Game
 
+# TODO should not be here!!!
+def find_start_and_goal_positions(spawns_per_map, free_positions):
+    max_attempts = 250
+    spawn_data = {}
+    used_pairs = set()
+
+    for i in range(spawns_per_map):
+        attempts = 0
+        while attempts < max_attempts:
+            # Randomly select start and goal from free positions
+            start_idx, goal_idx = np.random.choice(len(free_positions), 2, replace=False)
+            start = free_positions[start_idx]
+            goal = free_positions[goal_idx]
+
+            # Create a unique key for this pair (order matters)
+            pair_key = (start, goal)
+
+            if pair_key not in used_pairs:
+                used_pairs.add(pair_key)
+                spawn_data[i] = {"start": start, "goal": goal}
+                break
+
+            attempts += 1
+        else:
+            print(f"Warning: Could not find unique start-goal pair after {max_attempts} attempts")
+            break
+
+    return spawn_data
+
+def create_map(seed):
+    # TODO needed before
+    #seed = self.map_seed + self.current_map_index
+
+    random_generator = random.Random(seed)
+    np.random.seed(seed)
+
+    map = Map(
+        config.CONFIG["map"]["grid_width"],
+        config.CONFIG["map"]["grid_height"],
+        random_generator,
+        config.CONFIG["map"]["num_static_areas"],
+        config.CONFIG["map"]["num_dynamic_areas"]
+    )
+
+    return map
+
+def create_positions(map, spawns_per_map):
+    free_positions = map.get_free_positions()
+    max_possible_pairs = len(free_positions) * (len(free_positions) - 1)  # n * (n-1) since order matters
+    spawns_per_map = min(spawns_per_map, max_possible_pairs)
+
+    if spawns_per_map < spawns_per_map:
+        print(
+            f"Warning: Reduced spawns_per_map from {spawns_per_map} to {spawns_per_map} to respect map size constraints")
+
+    start_goal_pairs = find_start_and_goal_positions(spawns_per_map, free_positions)
+    return start_goal_pairs
+
+class TaskSpec:
+    def __init__(self, seed, position_index, map_index, game_map, position_pairs, agent_type):
+        self.seed = seed
+        self.position_index = position_index
+        self.map_index = map_index
+        self.map = copy.deepcopy(game_map)
+        self.position_pairs = position_pairs
+        self.agent_type = agent_type
+
+def create_tasks():
+    if config.CONFIG["seed"] is not None:
+        map_seed = config.CONFIG["seed"]
+    else:
+        map_seed = random.randint(0, 99999)
+
+    maps_to_test = config.CONFIG["maps_to_test"]
+    spawns_per_map = config.CONFIG["spawns_per_map"]
+    agent_types = config.CONFIG["agent_types"]
+
+    tasks = []
+    for current_agent_type in agent_types:
+        for map_index in tqdm(range(maps_to_test)):
+            seed = map_seed + map_index
+            current_map = create_map(seed)
+            start_goal_pairs = create_positions(current_map, spawns_per_map)
+            for key, pair in start_goal_pairs.items():
+                task = TaskSpec(seed=seed, position_index=key, map_index=map_index, game_map=current_map, position_pairs=pair, agent_type=current_agent_type)
+                tasks.append(task)
+
+            map_seed += 1
+
+    return tasks
+
+def export_runtime_data(agent_traces, map_traces):
+    name = config.CONFIG['experiment_name']
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_dir = os.path.join("outputs", name + "-" + timestamp)
+    os.makedirs(output_dir, exist_ok=True)
+
+    with open(os.path.join(output_dir, "agent_output.json"), "w") as f:
+        json.dump(agent_traces, f, indent=4)
+
+    with open(os.path.join(output_dir, "map_output.json"), "w") as f:
+        json.dump(map_traces, f, indent=4)
+
+    with open(os.path.join(output_dir, "used_config.yaml"), "w") as f:
+        yaml.dump(config.CONFIG, f, default_flow_style=False, sort_keys=False)
 
 def main(config):
+    index = 0
+    tasks = create_tasks()
+    if len(tasks) == 0:
+        return
+
     pygame.init()
     screen = pygame.display.set_mode((config["map"]["grid_width"] * config["map"]["cell_size"], config["map"]["grid_height"] * config["map"]["cell_size"]))
     pygame.display.set_caption("Map Generator & Path Planning Experiment")
     clock = pygame.time.Clock()
 
-    game = Game()
+    map_traces = {}
+    agent_traces = []
+
+    task = tasks[index]
+    game = Game(task)
+
+    fps = config["fps"]
+    background_color = config["color_background"]
 
     running = True
-    background_color = config["color_background"]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,7 +157,21 @@ def main(config):
         screen.fill(background_color)
         game.draw(screen)
         pygame.display.flip()
-        clock.tick(config["fps"])
+        clock.tick(fps)
+
+        if game.is_task_completed():
+            agent_trace, map_trace = game.get_trace()
+            map_traces[tasks[index].map_index] = map_trace
+            agent_traces.append(agent_trace)
+            index += 1
+            if index < len(tasks):
+                task = tasks[index]
+
+                game = Game(task)
+            else:
+                running = False
+
+    export_runtime_data(agent_traces, map_traces)
 
     pygame.quit()
 
@@ -39,6 +181,6 @@ if __name__ == "__main__":
     parser.add_argument("--schema", type=str, default="configs/schemas/project_schema.yaml", help="Schema file to validate the config")
     args = parser.parse_args()
 
-    config = load_config(args.config, args.schema)
+    project_config = load_config(args.config, args.schema)
 
-    main(config)
+    main(project_config)
