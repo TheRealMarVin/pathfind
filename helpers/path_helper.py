40e52d82dfa66d4c326cbcc48acdd15f4a314a65
therealmarvin <nakedtoothbrush@hotmail.com>
2025-07-09 20:19:45 -0400

    update utils for the path

--- helpers/path_helper.py
+++ helpers/path_helper.py
@@ -18,3 +18,19 @@ def compute_path_length(path):
         total += math.hypot(dx, dy)
 
     return total
+
+def is_touching(p1, p2):
+    if type(p1) is str or type(p2) is str:
+        return False
+
+    dx = abs(p1[0] - p2[0])
+    dy = abs(p1[1] - p2[1])
+
+    return not(dx > 1 or dy > 1)
+
+def count_invalid_moves(trace):
+    invalid_count = 0
+    for i in range(len(trace) - 1):
+        if not is_touching(trace[i], trace[i + 1]):
+            invalid_count += 1
+    return invalid_count
